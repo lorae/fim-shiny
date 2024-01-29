85016b2643102db5f1d59d88cf3d2d835ce3d461
Lorae <stojanovic.lorae@gmail.com>
2024-01-29 15:34:54 -0500

    refactor(app.R and example.env): adjust environment variables
    - Handle new FRED_API_KEY and WD_PATH variables
    - Remove references to deprecated OUT_PATH variable

--- app.R
+++ app.R
@@ -5,13 +5,13 @@ load_dot_env(file = ".env")
 
 # Ensure the read values are not null
 ## Add string constants which equal the key of the environment variables we need
-API_KEY <- Sys.getenv("API_KEY")
-OUT_PATH <- Sys.getenv("OUT_PATH")
+FRED_API_KEY <- Sys.getenv("FRED_API_KEY")
+WD_PATH <- Sys.getenv("WD_PATH")
 
-if ((API_KEY == "") || (OUT_PATH == "")) {
+if ((FRED_API_KEY == "") || (WD_PATH == "")) {
   print("ERROR: Environment variable(s) returned NA")
   quit(status = 1)
 }
 
-# print(paste("My API Key: ", API_KEY))
-# print(paste("My output path: ", OUT_PATH))
+print(paste("My FRED API Key: ", FRED_API_KEY))
+print(paste("My output path: ", WD_PATH))
